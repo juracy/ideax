ff4b7c6893c5a93ef21c4cfa47704b43b76dbb1a
samyrabdo <samyr.info@gmail.com>
2019-02-01 14:10:43 -0200

    changing allowed_hosts

--- ideax/settings/django/_core.py
+++ ideax/settings/django/_core.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 GOOGLE_RECAPTCHA_SECRET_KEY = config('GOOGLE_RECAPTCHA_SECRET_KEY', default='')
 
